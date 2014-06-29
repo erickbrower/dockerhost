0b3f5eb25e30d71383324c8d442fba45bef63a0d
Erick Brower <cerickbrower@gmail.com>
2014-06-29 08:07:43 -0700

    Fixing set_exports

--- install.sh
+++ install.sh
@@ -11,17 +11,18 @@ git clone https://github.com/erickbrower/dockerhost.git $DOCKERHOST
 (cd $DOCKERHOST && vagrant up)
 
 function _set_exports() {
+    echo 'Setting exports for DOCKERHOST in $1'
     echo 'export DOCKER_HOST=tcp://localhost:2375' >> $1
     echo 'export DOCKERHOST=~/.dockerhost' >> $1
     . $1
 }
 
 if [ -f "~/.bashrc" ]; then
-    _set_exports ~/.bashrc
+    _set_exports '~/.bashrc'
 fi
 
 if [ -f "~/.zshrc" ]; then
-    _set_exports ~/.zshrc
+    _set_exports '~/.zshrc'
 fi
 
 echo "Done! Try running a command like 'docker pull erickbrower/rails'"
